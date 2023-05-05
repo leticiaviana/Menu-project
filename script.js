39e7d0420bbb40baddb8f92cc126609ade84bd05
leticia_paixao <leticia.paixao@informata.com.br>
2023-05-05 12:20:03 -0300

    :sparkles: add script JS file

---
+++ script.js
@@ -0,0 +1,66 @@
+const menu = [
+    {
+        id: 1,
+        title: "buttermilk pancakes",
+        price: 15.99,
+        img: "",
+        desc: `I'm baby woke milkshake wolf bitters live-edge 
+        blue bottle, hammok freegan copper mug whatever cold pressed`,
+    },  
+    {
+        id: 2,
+        title: "buttermilk pancakes2",
+        price: 15.99,
+        img: "",
+        desc: `I'm baby woke milkshake wolf bitters live-edge 
+        blue bottle, hammok freegan copper mug whatever cold pressed`,
+    },  
+    {
+        id: 3,
+        title: "buttermilk pancakes3",
+        price: 15.99,
+        img: "",
+        desc: `I'm baby woke milkshake wolf bitters live-edge 
+        blue bottle, hammok freegan copper mug whatever cold pressed`,
+    },
+    {
+        id: 4,
+        title: "buttermilk pancakes4",
+        price: 15.99,
+        img: "",
+        desc: `I'm baby woke milkshake wolf bitters live-edge 
+        blue bottle, hammok freegan copper mug whatever cold pressed`,
+    },
+    {
+        id: 5,
+        title: "buttermilk pancakes5",
+        price: 15.99,
+        img: "",
+        desc: `I'm baby woke milkshake wolf bitters live-edge 
+        blue bottle, hammok freegan copper mug whatever cold pressed`,
+    },
+];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function(){
+    let displayMenu = menu.map(function(item){
+        // console.log(item);
+        return `<article class="menu-item">
+        <img src="${item.img}" class="phtoto" alt="${item.title}">
+        <div class=" ">
+            <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">
+                ${item.desc}
+            </p>
+
+        </div>
+    </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    console.log(displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+})
